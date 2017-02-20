44b4c7bf38e0a682dc607f1e17ee2f8647b7ac4a
jhw84 <jhweijs@gmail.com>
2017-02-20 22:44:54 +0100

    Initial Commit
    Initial

---
+++ basic1/basic1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  basic1
+//
+//  Created by Joost Weijs on 20/02/2017.
+//  Copyright © 2017 Joost Weijs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ basic1/basic1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  basic1
+//
+//  Created by Joost Weijs on 20/02/2017.
+//  Copyright © 2017 Joost Weijs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
